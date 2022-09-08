9f6bacc7e6d6005ba723171d7d6a1524e3006667
Your Name <you@example.com>
2022-09-08 14:35:01 -0700

    C program that prints exactly

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,2 +1,14 @@
-#!/bin/bash
+#include <stdio.h>
 
+/**
+ * main - print the string in the put function
+ *
+ * Description: using the main function
+ * this program prints "with proper grammer,but the outcome is a piece of art,"
+ * Return: 0
+ */
+int main(void)
+{
+	printf("with proper grammer, but the outcome is a piece of art,\n");
+	Return (0);
+}
