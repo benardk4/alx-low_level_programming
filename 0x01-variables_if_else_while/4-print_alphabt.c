e10f5e080f9fe2153a77f5abc6412b68a6ff0da9
Your Name <you@example.com>
2022-09-09 16:36:20 -0700

    Task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: using the main function
+ * this program prints all alphabets except e and q
+ * Return: 0
+ */
+int main(void)
+	{
+		char ch;
+
+		for (ch = 'a' ; ch <= 'z' ; ch++)
+		{
+			if (ch !='e' && ch !='q')
+			{
+				putchar(ch);
+			}
+		}
+		putchar('\n');
+		return (0);
+	}
