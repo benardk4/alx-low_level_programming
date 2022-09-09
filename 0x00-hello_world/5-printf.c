8083229ae7f4ebac16a483c624b84d80c2fa28f2
Your Name <you@example.com>
2022-09-09 07:56:05 -0700

    Task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - print the string in the printf function
+ * main - print the string in the put function
  *
  * Description: using the main function
  * this program prints "with proper grammar, but the outcome is a piece of art,"
