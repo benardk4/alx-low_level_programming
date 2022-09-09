9c315e44a9cfe7ec088f99e2df79ab453f6901e6
Your Name <you@example.com>
2022-09-09 05:42:46 -0700

    Task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 /**
- * main - print the string in the put function
+ * main - print the string in the printf function
  *
  * Description: using the main function
- * this program prints "with proper grammer,but the outcome is a piece of art,"
+ * this program prints "with proper grammar, but the outcome is a piece of art,"
  * Return: 0
  */
 int main(void)
