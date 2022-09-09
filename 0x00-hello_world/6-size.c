9939abd9b87bab632c59f5c02a05f93f0344d704
Your Name <you@example.com>
2022-09-09 01:26:38 -0700

    C program that prints the size of various types on the computer it is compiled and run on

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,25 +1,20 @@
 #include <studio.h>
 
 /**
- * main - print the string in the put function
+ * main - Entry point
  *
- * Desccription: using the main function
- * this program prints "Hello
- * Return: 0
+ * Description: prints the sizes of various data types
+ *
+ * Return: Always 0 (Success)
+ *
  */
 int main(void)
 {
-	char c
-	int i
-	long li;
-	long long lli;
-	float f
-
-	printf("Size of a char:%ld byte(s)\n", sizeof(c));
-	printf("Size of an int:%ld byte(s)\n", sizeof(i));
-	printf("Size of a long int:%ld byte(s)\n", sizeof(li));
-	printf("Size of long long int:%ld byte(s)\n", sizeof(lli));
-	printf("Size of a float:%ld byte(s)\n", sizeof(f));
+	printf("Size of a char: %d byte(s)\n", (int)sizeof(char));
+	printf("Size of an int: %d byte(s)\n", (int)sizeof(int));
+	printf("Size of a long int: %d byte(s)\n", (int)sizeof(long int));
+	printf("Size of long long int: %d byte(s)\n", (int)sizeof(long long int));
+	printf("Size of a float: %d byte(s)\n", (int)sizeof(float));
 	return(0);
 }
 
