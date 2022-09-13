15b9209e06c7658a9cdb959e88a9458ec9172ac0
Your Name <you@example.com>
2022-09-13 15:11:21 -0700

    Task 4

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,7 +2,6 @@
 /**
  * _isapha - function to check if c is a letter, lowercase or uppercase
  * @c: is the int that will use for the argument of the function
- * Description: check if c is a lowercase or uppercase letter
  * Return: 0
  */
 int _isalpha(int c)
