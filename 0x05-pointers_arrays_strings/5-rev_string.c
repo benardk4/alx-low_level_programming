e3197c073efa2ea3829678c87678cd91a6364006
Bernard Kadima <benardk4@gmail.com>
2022-09-20 23:22:28 +0300

    Task 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * rev_string -Reverse a string
+ * @s: string to be reversed
+ * Return: nothing
+ */
+
+void rev_string(char *s)
+{
+	int i, tmp, len = _strlen(s);
+
+	for (i = 0; i < len/s; i++)
+	{
+		tmp = *(s + i);
+		*(s + i) = *(s + len - i - 1);
+		*(s + len - i - 1) = tmp;
+	}
+}
