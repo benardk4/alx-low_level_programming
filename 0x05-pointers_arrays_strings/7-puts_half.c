4e3eb4e94342fcd595301d0321f29401825140b7
Bernard Kadima <benardk4@gmail.com>
2022-09-20 23:57:05 +0300

    Task 7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -12,7 +12,7 @@ void puts_half(char *str)
 {
 	int i, len = _strlen(str);
 
-	for (i = ((len - 1) / 2) + 1; i < len; i++);
+	for(i = ((len - 1) / 2) + 1; i < len; i++);
 			putchar(*(str + i));
 			putchar(10);
 }
@@ -26,10 +26,10 @@ void puts_half(char *str)
 
 int _strlen(char *s)
 {
-int len = 0;
+	int len = 0;
 
-while (*(s + len) != '\0');
-len++;
+	while(*(s + len) != '\0');
+	len++;
 
-return (len);
+	return (len);
 }
