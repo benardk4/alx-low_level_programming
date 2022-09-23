7c99e3a5d27e426aaf0e78318e982368a7b4d6f1
Bernard Kadima <benardk4@gmail.com>
2022-09-23 20:29:06 +0000

    Task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,50 +1,33 @@
 #include "main.h"
 
 /**
- * _strlen - returns the length of a string
- * @s: string
- * Return: returns length as integer
+ * cap_string - capitalizes all words in a string
+ * @s: string to capitalize
+ *
+ * Return: address of s
  */
 
-int _strlen(char *s)
+char *cap_string(char *s)
 {
-	int len 0;
+	int i = 0, j;
+	char a[] = " \t\n,;.!?\" (){}";
 
-	while (*(s + len) != '\0')
-		len++;
-
-	return (len);
-}
-/**
- * cap_string - function that captalize first character of a word
- * @str: string to captalize
- * Return: returns the capitalized string
-*/
-
-char *cap_string(char *str)
-{
-	int index = 0;
-
-	while (str[++index])
+	while (*(s + i))
 	{
-		while (!(str[index] >= 'a') && (str[index] <= 'z'))
-			index++;
-		if (str[index - 1] == ' ' ||
-				str[index - 1] == '\t' ||
-				str[index - 1] == '\n' ||
-				str[index - 1] == ',' ||
-				str[index - 1] == ';' ||
-				str[index - 1] == '!' ||
-				str[index - 1] == '"' ||
-				str[index - 1] == '.' ||
-				str[index - 1] == '?' ||
-				str[index - 1} == '(' ||
-				str[index - 1] == ')' ||
-				str[index - 1] == '}' ||
-				str[index - 1] == '{')
-			str[index] -= 32;
-}
-
-return (str);
-}
-
+		if (*(s + i) >= 'a' && *(s + i) <= 'z')
+		{
+			if (i == 0)
+				*(s + i) -= 'a' - 'A';
+			else
+			{
+				for (j = 0; j <= 12; j++)
+				{
+					if (a[j] == *(s + i - 1)
+						*(s + i) -= 'a' - 'A';
+						}
+						}
+						}
+						i++;
+						}
+						return (s);
+						}
